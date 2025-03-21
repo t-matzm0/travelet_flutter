0c076b50e48aacbb2333c000649a9ac6d377d051
Takashi Matsumoto <lonelydarklark@gmail.com>
2025-03-21 16:31:48 +0900

    前回のコミット漏れ

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '旅行プランアプリ',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: HomeScreen(),
     );
   }
